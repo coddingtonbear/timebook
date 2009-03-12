ca3e566615e5978fa983f43a3e1a9903cdc28305
Trevor Caira <trevor@caira.com>
2009-03-12 04:05:53 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+from timebook import __version__
+
+setup(
+    name='timebook',
+    version='.'.join(str(bit) for bit in __version__),
+    url='http://bitbucket.org/trevor/timebook/',
+    description='Simple time tracking utility',
+    author='Trevor Caira',
+    author_email='trevor@caira.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['timebook'],
+    scripts=['t'],
+)

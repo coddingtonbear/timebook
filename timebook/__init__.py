9b73f74236430f1254f74feda3867d421b2aff97
Adam Coddington <me@adamcoddington.net>
2012-04-29 11:45:41 -0700

    Removing unnecessary path calculations; lets leave that code to the web app.

--- timebook/__init__.py
+++ timebook/__init__.py
@@ -21,56 +21,15 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from ConfigParser import ConfigParser
-import base64
-import datetime
-import getpass
-import json
 import logging
 import os.path
-import re
-import sqlite3
 import sys
-import time
-import urllib2
 
 __author__ = 'Trevor Caira <trevor@caira.com>, Adam Coddington <me@adamcoddington.net>'
 __version__ = (3, 0, 0)
 
 def get_version():
     return '.'.join(str(bit) for bit in __version__)
-
-def get_user_path(guess):
-    """
-    Using a supplied username, get the homedir path.
-    """
-    return os.path.join(HOME_DIR, guess)
-
-def get_best_user_guess():
-    """
-    Searches for the most recently modified timebook database to find the
-    most reasonable user.
-    """
-    dirs = os.listdir(HOME_DIR)
-    max_atime = 0;
-    final_user = None;
-    for homedir in dirs:
-        timebook_db_file = os.path.join(
-                        HOME_DIR,
-                        homedir,
-                        ".config/timebook/sheets.db"
-                    )
-        if os.path.exists(timebook_db_file):
-            if os.stat(timebook_db_file).st_atime > max_atime:
-                final_user = homedir
-    if not final_user:
-        final_user = getpass.getuser()
-    return final_user
-
-if sys.platform == 'darwin':
-    HOME_DIR = "/Users"
-elif sys.platform == 'linux2':
-    HOME_DIR = "/home"
 
 logger = logging.getLogger('timebook')
 logger.setLevel(logging.DEBUG)
@@ -79,10 +38,6 @@ handler = logging.StreamHandler(sys.stderr)
 handler.setLevel(logging.INFO)
 logger.addHandler(handler)
 
-CONFIG_DIR = os.path.expanduser(os.path.join(
-        get_user_path(
-            get_best_user_guess()
-            )
-    , ".config", "timebook"))
+CONFIG_DIR = os.path.expanduser('~/.config/timebook'))
 CONFIG_FILE = os.path.join(CONFIG_DIR, "timebook.ini")
 TIMESHEET_DB = os.path.join(CONFIG_DIR, "sheets.db")

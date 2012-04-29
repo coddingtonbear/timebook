9b10c811e5a0c0c00adadb4f23d9be98b74f920a
Adam Coddington <me@adamcoddington.net>
2012-04-29 01:15:26 -0700

    Adding self to get_hours_details now that it is a class member.

--- timebook/payperiodutil.py
+++ timebook/payperiodutil.py
@@ -20,7 +20,7 @@ class PayPeriodUtil(object):
         if not self.weekdays_rule:
             self.weekdays_rule = rrule.rrule(rrule.DAILY, byweekday=(rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR, ), dtstart=begin_period)
 
-    def get_hours_details():
+    def get_hours_details(self):
         all_weekdays = self.weekdays_rule.between(begin_period, end_period)
         expected_hours = self.hours_per_day * len(all_weekdays)
         unpaid = 0

344de68c6e638c8ebc81a858379df3a51234de16
Adam Coddington <me@adamcoddington.net>
2012-05-25 15:19:57 -0700

    Also build timebook.migrations module.  Co-worker Slaby discovered this bug.
    --HG--
    extra : rebase_source : 11147047d8d7218524e6f3e7534152169f955c52

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Utilities',
     ],
-    packages=['timebook', ],
+    packages=['timebook', 'timebook.migrations',],
     entry_points={'console_scripts': [
         't = timebook.cmdline:run_from_cmdline']},
     install_requires = [
